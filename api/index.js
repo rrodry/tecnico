af74060279714bb453c95a6d9ea3cd6e32070eaf
Rodrigo Rimbau <39181740+rrodry@users.noreply.github.com>
2022-11-16 06:38:13 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,10 +1,32 @@
-const server = require("./src/app");
-const { conn } = require("./src/db.js");
-const { PORT } = process.env;
-
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const  axios  = require('axios')
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT || PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`); // eslint-disable-line no-console
-  });
+async function getAPI() {
+  await axios.get("http://localhost:3001/regions")
+}
+conn.sync({ force: true }).then(() => {
+  getAPI()
+  server.listen(3001, () => {
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  })
 });
